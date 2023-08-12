6429e744948713c210db81c52ed26793e9f021db
Andrew Leonov <andrewleonov999@gmail.com>
2023-08-12 03:42:47 +0300

    Log In Functionality

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -19,8 +19,12 @@ struct LoginView: View {
                            angle: 15,
                            background: .pink)
                 
-                // Login Form
                 Form {
+                    if !viewModel.errorMessage.isEmpty {
+                        Text(viewModel.errorMessage)
+                            .foregroundColor(Color.red)
+                    }
+                    
                     TextField("Email Address", text: $viewModel.email)
                         .textFieldStyle(DefaultTextFieldStyle())
                         .autocapitalization(.none)
@@ -32,7 +36,7 @@ struct LoginView: View {
                         title: "Log In",
                         background: .blue
                     ) {
-                        // Attempt log in
+                        viewModel.login()
                     }
                     .padding()
                 }
